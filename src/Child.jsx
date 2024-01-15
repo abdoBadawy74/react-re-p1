957b6f6e63010e38d5c8c31539ecb5b29c917fc5
abd074 <badawyabdulrahman099@gmail.com>
2024-01-15 02:53:21 +0200

    onSale | delete

--- src/Child.jsx
+++ src/Child.jsx
@@ -2,14 +2,23 @@ import React, { Component } from "react";
 
 export default class Child extends Component {
   render() {
-    let { name, price, count } = this.props.productInfo;
+    let { name, price, onSale, count,id } = this.props.productInfo;
     return (
-      <div className="col-md-3 bg-light p-2">
+      <div className="col-md-3 p-2">
         {/* <h6>name from parent is {this.props.productInfo.name}</h6> */}
-        <h6>name from parent is {name}</h6>
-        <h6>price from parent is {price}</h6>
-        <h6>count from parent is {count}</h6>
-        
+        <div className="bg-light h-100 p-2 position-relative">
+          <h6>name from parent is {name}</h6>
+          <h6>price from parent is {price}</h6>
+          <h6>count from parent is {count}</h6>
+          {onSale == true ? (
+            <div className="sale p-1 bg-danger text-white position-absolute top-0 end-0">
+              Sale
+            </div>
+          ) : (
+            ""
+          )}
+          <button onClick={()=>this.props.delete(id)} className="btn btn-outline-danger w-100">Delete</button>
+        </div>
       </div>
     );
   }
--- src/Parent.jsx
+++ src/Parent.jsx
@@ -3,19 +3,39 @@ import Child from "./Child";
 
 export default class Parent extends Component {
   state = {
-    product: {
-      id: 1,
-      name: "Toshipa c3",
-      price: 8000,
-      onSale: false,
-      count: 10,
-    },
+    products: [
+      { id: 1, name: "Toshipa c3", price: 8000, onSale: false, count: 10 },
+      { id: 2, name: "oppo", price: 8000, onSale: true, count: 13 },
+      { id: 3, name: "samsung 173 c3", price: 8000, onSale: false, count: 35 },
+      { id: 4, name: "National c54", price: 8000, onSale: true, count: 2 },
+      { id: 5, name: "LG y3", price: 8000, onSale: false, count: 7 },
+      { id: 6, name: "Apple macBook", price: 8000, onSale: true, count: 8 },
+      { id: 7, name: "Toshipa c3", price: 8000, onSale: false, count: 10 },
+      { id: 8, name: "oppo", price: 8000, onSale: true, count: 13 },
+      { id: 9, name: "samsung 173 c3", price: 8000, onSale: false, count: 35 },
+      { id: 10, name: "National c54", price: 8000, onSale: true, count: 2 },
+      { id: 11, name: "LG y3", price: 8000, onSale: false, count: 7 },
+      { id: 12, name: "Apple macBook", price: 8000, onSale: true, count: 8 },
+    ],
   };
+
+  deleteProduct = (id) => {
+    // deep copy
+    let newProducts = [...this.state.products];
+    //action
+    newProducts = newProducts.filter((product) => product.id !== id);
+    //setState
+    this.setState({products:newProducts});
+  };
+
   render() {
     return (
-      <div className="bg-dark p-3">
-        <h3 className="text-white">Parent</h3>
-        <Child productInfo={this.state.product}></Child>
+      <div className="container bg-dark py-4">
+        <div className="bg-dark p-3 row">
+          {this.state.products.map((product, i) => (
+            <Child key={i} delete={this.deleteProduct} productInfo={product}></Child>
+          ))}
+        </div>
       </div>
     );
   }
